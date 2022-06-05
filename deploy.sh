5d804296253e911c1da00353fbfcdd48093eb144
yujinhui <1265921100@qq.com>
2022-06-05 14:20:07 +0800

    容器部署和更新工具

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 容器部署和更新脚本
+
+set -euo pipefail
+
+# 拉取最新代码
+git pull
+
+# 构建镜像
+for i in `seq 5`
+do
+    docker image build -t nginx:webserver --memory 2g . && break
+done
+
+# 若容器存在则删除
+docker container inspect jinhui.dev &>/dev/null && docker container rm -f jinhui.dev
+
+# 启动容器
+docker container run --name jinhui.dev -p80:80 -p443:443 --restart always --memory 1g -d nginx:webserver
