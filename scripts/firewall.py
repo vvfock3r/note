e7761255c40c5a017b7f6e598c8ad3da7a7be840
yujinhui <1265921100@qq.com>
2022-07-17 17:47:52 +0800

    添加版本信息和调整选项顺序

--- scripts/firewall.py
+++ scripts/firewall.py
@@ -17,6 +17,8 @@ from tencentcloud.common.credential import Credential
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.lighthouse.v20200324 import lighthouse_client, models
 
+__version__ = "v0.0.1"
+
 
 def get_internet_ip() -> Union[str, None]:
     '''获取当前外网出口IP'''
@@ -189,6 +191,49 @@ def del_firewall_rule(
         return False, {"code": err.code, "message": err.message, "requestId": err.requestId}
 
 
+def format_group_order(parser: argparse.ArgumentParser, items: list):
+    """调整组顺序，items数据结构为: [ (组名1,放到哪个索引上), (组名2,放到哪个索引上) ]
+    索引从1开始
+    """
+
+    for group_name, index in items:
+        for _index, group in enumerate(parser._action_groups.copy()):
+            if group.title == group_name:
+                # 删除组并重新添加
+                del parser._action_groups[_index]
+                parser._action_groups.insert(index, group)
+
+
+def format_param_order(parser: argparse.ArgumentParser, items: list):
+    """调整参数顺序，items数据结构为: [ (组名1, 参数1, 放到哪个索引上), (组名2, 参数2, 放到哪个索引上) ]"""
+
+    for group_name, option_name, index in items:
+        # 先找到组
+        for group in parser._action_groups.copy():
+            if group.title == group_name:
+                # 在组内找到参数
+                for _index, action in enumerate(group._group_actions.copy()):
+                    if option_name in action.option_strings:
+                        # 删除参数,并重新添加
+                        del group._group_actions[_index]
+                        group._group_actions.insert(index, action)
+
+
+def format_help_string(parser: argparse.ArgumentParser, items: list):
+    '''使用字符串替换的方式调整输出内容'''
+
+    # 将原始的format_help函数备份一下,后面会用到
+    parser._original_format_help = parser.format_help
+
+    def wrapper():
+        ret = parser._original_format_help()
+        for old, new in items:
+            ret = ret.replace(old, new)
+        return ret
+
+    parser.format_help = wrapper
+
+
 def main():
     # 根命令和子命令
     parser = argparse.ArgumentParser(usage="%(prog)s Command [options]")
@@ -196,6 +241,12 @@ def main():
     addCommand = subparsers.add_parser("add", help="add firewall rule")
     delCommand = subparsers.add_parser("del", help="delete firewall rule")
     lsCommand = subparsers.add_parser("ls", help="query firewall rule")
+
+    # 根命令参数
+    parser.add_argument("-v", "--version", default=argparse.SUPPRESS,
+                        help="show the version of %(prog)s and exit", action="store_true")
+    parser.add_argument("-q", "--quiet", default=argparse.SUPPRESS,
+                        help="do not print response messages", action="store_true")
 
     # 子命令公共参数
     addCommand.add_argument("--secretId",
@@ -309,6 +360,17 @@ def main():
                            help="description (default: %(default)s)")
     lsCommand.set_defaults(func=ls_firewall_rule)
 
+    # 自定义帮助信息,如果有问题这一段可以删除，不影响代码执行
+    format_help_string(parser, [
+        ("{add,del,ls}\n", "\r"),
+        ("  add  ", "add    "),
+        ("  del  ", "del    "),
+        ("  ls  ", "ls    "),
+    ])  # 根命令
+    format_param_order(addCommand, [("optional arguments", "--help", 9)])
+    format_param_order(delCommand, [("optional arguments", "--help", 9)])
+    format_param_order(lsCommand, [("optional arguments", "--help", 9)])
+
     # 解析参数
     args = parser.parse_args()
 
@@ -316,6 +378,14 @@ def main():
     if len(args.__dict__) <= 0:
         parser.print_help()
         sys.exit(1)
+
+    # 查看版本
+    if args.__dict__.get("version"):
+        print(__version__)
+        sys.exit(0)
+
+    # 静默输出
+    quiet = args.__dict__.pop("quiet", False)
 
     # 解析IPv4
     if args.__dict__.get("source") == "dynamic":
@@ -326,7 +396,12 @@ def main():
 
     # 执行函数
     ok, msg = args.__dict__.pop("func")(cred=cred, **args.__dict__)
-    print(json.dumps(msg, ensure_ascii=False, indent=4))
+
+    # 非静默输出(没有指定-q)
+    if not quiet:
+        print(json.dumps(msg, ensure_ascii=False, indent=4))
+
+    # 退出程序
     sys.exit(0 if ok else 1)
 
 
