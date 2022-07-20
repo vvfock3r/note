30fcbdea666debaa09984e2753be245bb6130eff
yujinhui <1265921100@qq.com>
2022-07-20 14:38:33 +0800

    添加region参数

--- scripts/firewall.py
+++ scripts/firewall.py
@@ -22,12 +22,12 @@ __version__ = "v0.0.1"
 
 class FirewallRule:
     def __init__(
-        self,
-        protocol: Optional[str] = None,
-        port: Optional[str] = None,
-        source: Optional[str] = None,
-        action: Optional[str] = None,
-        description: Optional[str] = None,
+            self,
+            protocol: Optional[str] = None,
+            port: Optional[str] = None,
+            source: Optional[str] = None,
+            action: Optional[str] = None,
+            description: Optional[str] = None,
     ):
         self.protocol = protocol
         self.port = port
@@ -36,80 +36,23 @@ class FirewallRule:
         self.description = description
 
 
-def add_firewall_rule(cred: Credential, instance_id: str, rule: FirewallRule) -> Tuple[bool, Dict]:
-    # 先执行查询,description不计数
-    rule2 = copy.deepcopy(rule)
-    rule2.description = None
-    ok, resp = ls_firewall_rule(cred, instance_id, rule2)
-
-    # 查询失败直接返回
-    if not ok:
-        return ok, resp
-
-    # 如果规则已经存在直接返回
-    rules_matched = resp["FirewallRuleSet"]
-    if len(rules_matched) >= 1:
-        return True, {
-            "code": "ok",
-            "message": "防火墙规则 `[('{protocol}', '{port}', '{source}', '{action}')]` 已经存在。".format(
-                protocol=rule.protocol,
-                port=rule.port,
-                source=rule.source,
-                action=rule.action,
-            ),
-            "requestId": resp["RequestId"],
-        }
-
-    # 添加规则
-    try:
-        # 实例化客户端
-        client = lighthouse_client.LighthouseClient(cred, "ap-hongkong")
-
-        # 生成请求
-        req = models.CreateFirewallRulesRequest()
-
-        # 填充请求参数
-        params = {
-            "InstanceId": instance_id,
-            "FirewallRules": [
-                {
-                    "Protocol": rule.protocol,
-                    "Port": rule.port,
-                    "CidrBlock": rule.source,
-                    "Action": rule.action,
-                    "FirewallRuleDescription": rule.description,
-                }
-            ],
-        }
-        req.from_json_string(json.dumps(params))
-
-        # 发送请求
-        resp = client.CreateFirewallRules(req)
-
-        return True, {
-            "code": "ok",
-            "message": "Add 1 rules",
-            "requestId": resp.RequestId,
-        }
-    except TencentCloudSDKException as err:
-        return False, {
-            "code": err.code,
-            "message": err.message,
-            "requestId": err.requestId,
-        }
+class Instance:
+    def __init__(self, id: str, region: str):
+        self.id = id
+        self.region = region
 
 
-def ls_firewall_rule(cred: Credential, instance_id: str, rule: FirewallRule) -> Tuple[bool, Dict]:
+def ls_firewall_rule(cred: Credential, instance: Instance, rule: FirewallRule) -> Tuple[bool, Dict]:
     """注意：只能查看100条规则，本函数没有实现分页功能"""
     try:
         # 实例化客户端
-        client = lighthouse_client.LighthouseClient(cred, "ap-hongkong")
+        client = lighthouse_client.LighthouseClient(cred, instance.region)
 
         # 生成请求
         req = models.DescribeFirewallRulesRequest()
 
         # 填充请求参数
-        params = {"InstanceId": instance_id, "Limit": 100}
+        params = {"InstanceId": instance.id, "Limit": 100}
         req.from_json_string(json.dumps(params))
 
         # 发送请求
@@ -147,9 +90,72 @@ def ls_firewall_rule(cred: Credential, instance_id: str, rule: FirewallRule) -> 
         }
 
 
-def remove_firewall_rule(cred: Credential, instance_id: str, rule: FirewallRule) -> Tuple[bool, Dict]:
+def add_firewall_rule(cred: Credential, instance: Instance, rule: FirewallRule) -> Tuple[bool, Dict]:
+    # 先执行查询,description不计数
+    rule2 = copy.deepcopy(rule)
+    rule2.description = None
+    ok, resp = ls_firewall_rule(cred, instance, rule2)
+
+    # 查询失败直接返回
+    if not ok:
+        return ok, resp
+
+    # 如果规则已经存在直接返回
+    rules_matched = resp["FirewallRuleSet"]
+    if len(rules_matched) >= 1:
+        return True, {
+            "code": "ok",
+            "message": "防火墙规则 `[('{protocol}', '{port}', '{source}', '{action}')]` 已经存在。".format(
+                protocol=rule.protocol,
+                port=rule.port,
+                source=rule.source,
+                action=rule.action,
+            ),
+            "requestId": resp["RequestId"],
+        }
+
+    # 添加规则
+    try:
+        # 实例化客户端
+        client = lighthouse_client.LighthouseClient(cred, instance.region)
+
+        # 生成请求
+        req = models.CreateFirewallRulesRequest()
+
+        # 填充请求参数
+        params = {
+            "InstanceId": instance.id,
+            "FirewallRules": [
+                {
+                    "Protocol": rule.protocol,
+                    "Port": rule.port,
+                    "CidrBlock": rule.source,
+                    "Action": rule.action,
+                    "FirewallRuleDescription": rule.description,
+                }
+            ],
+        }
+        req.from_json_string(json.dumps(params))
+
+        # 发送请求
+        resp = client.CreateFirewallRules(req)
+
+        return True, {
+            "code": "ok",
+            "message": "Add 1 rules",
+            "requestId": resp.RequestId,
+        }
+    except TencentCloudSDKException as err:
+        return False, {
+            "code": err.code,
+            "message": err.message,
+            "requestId": err.requestId,
+        }
+
+
+def remove_firewall_rule(cred: Credential, instance: Instance, rule: FirewallRule) -> Tuple[bool, Dict]:
     # 先执行查询
-    ok, resp = ls_firewall_rule(cred, instance_id, rule)
+    ok, resp = ls_firewall_rule(cred, instance, rule)
 
     # 查询失败直接返回
     if not ok:
@@ -167,13 +173,13 @@ def remove_firewall_rule(cred: Credential, instance_id: str, rule: FirewallRule)
     # 再执行删除
     try:
         # 实例化客户端
-        client = lighthouse_client.LighthouseClient(cred, "ap-hongkong")
+        client = lighthouse_client.LighthouseClient(cred, instance.region)
 
         # 生成请求
         req = models.DeleteFirewallRulesRequest()
 
         # 填充请求参数
-        params = {"InstanceId": instance_id, "FirewallRules": resp["FirewallRuleSet"]}
+        params = {"InstanceId": instance.id, "FirewallRules": resp["FirewallRuleSet"]}
         req.from_json_string(json.dumps(params))
 
         # 发送请求
@@ -205,20 +211,31 @@ class Cli:
         """获取当前外网出口IP"""
 
         urls = ["https://api-ipv4.ip.sb/ip"]
-        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
         for url in urls:
-            r = httpx.get(url, headers=headers, timeout=30)
-            if r.status_code == 200:
-                return r.text.strip("\n")
+            for i in range(3):
+                r = httpx.get(url, headers=headers, timeout=10)
+                if r.status_code == 200:
+                    return r.text.strip("\n")
 
         return None
 
     @staticmethod
-    def run(func, kwargs) -> None:
+    def request(func, kwargs) -> None:
         """真正的运行逻辑"""
 
-        # 创建认证信息
-        kwargs["cred"] = Credential(kwargs.pop("secret_id"), kwargs.pop("secret_key"))
+        # 实例化认证对象
+        kwargs["cred"] = Credential(
+            secret_id=kwargs.pop("secret_id"),
+            secret_key=kwargs.pop("secret_key")
+        )
+
+        # 实例化Instance对象
+        kwargs["instance"] = Instance(
+            id=kwargs.pop("instance_id"),
+            region=kwargs.pop("region")
+        )
 
         # 创建防火墙规则
         if kwargs["source"] == "current ip":
@@ -260,6 +277,7 @@ class Cli:
     @click.option("--secret-id", required=True, help="Secret id")
     @click.option("--secret-key", required=True, help="Secret key")
     @click.option("--instance-id", required=True, help="Instance id")
+    @click.option("--region", required=True, help="Region, eg: ap-hongkong")
     @click.option("--port", help="Port, supported, - and ALL")
     @click.option("--protocol", help="TCP, UDP, ICMP, ALL")
     @click.option("--source", help="CIDR")
@@ -271,12 +289,13 @@ class Cli:
     def ls(**kwargs):
         """query firewall rule for tencent cloud lighthouse"""
 
-        Cli.run(ls_firewall_rule, kwargs)
+        Cli.request(ls_firewall_rule, kwargs)
 
     @parser.command()
     @click.option("--secret-id", required=True, help="Secret id")
     @click.option("--secret-key", required=True, help="Secret key")
     @click.option("--instance-id", required=True, help="Instance id")
+    @click.option("--region", required=True, help="Region, eg: ap-hongkong")
     @click.option("--port", required=True, help="Port, supported, - and ALL")
     @click.option("--protocol", help="TCP, UDP, ICMP, ALL", default="TCP", show_default=True)
     @click.option("--source", help="CIDR", default="current ip", show_default=True)
@@ -288,12 +307,13 @@ class Cli:
     def add(**kwargs):
         """add firewall rule for tencent cloud lighthouse"""
 
-        Cli.run(add_firewall_rule, kwargs)
+        Cli.request(add_firewall_rule, kwargs)
 
     @parser.command()
     @click.option("--secret-id", required=True, help="Secret id")
     @click.option("--secret-key", required=True, help="Secret key")
     @click.option("--instance-id", required=True, help="Instance id")
+    @click.option("--region", required=True, help="Region, eg: ap-hongkong")
     @click.option("--port", help="Port, supported, - and ALL")
     @click.option("--protocol", help="TCP, UDP, ICMP, ALL")
     @click.option("--source", help="CIDR")
@@ -305,7 +325,7 @@ class Cli:
     def remove(**kwargs):
         """remove firewall rule for tencent cloud lighthouse"""
 
-        Cli.run(remove_firewall_rule, kwargs)
+        Cli.request(remove_firewall_rule, kwargs)
 
 
 if __name__ == "__main__":
